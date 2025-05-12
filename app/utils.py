99311cbc4e72dd91af69c03867a7496324dff02d
Saayella <saayella@gmail.com>
2025-05-12 08:36:32 +0800

    Added Media Type Breakdown Analysis

---
+++ app/utils.py
@@ -0,0 +1,23 @@
+from app.models import MediaEntry
+from app import db
+from sqlalchemy import func
+
+def get_media_type_breakdown(user_id):
+    counts = (
+        db.session.query(MediaEntry.media_type, func.count(MediaEntry.id))
+        .filter_by(user_id=user_id)
+        .group_by(MediaEntry.media_type)
+        .all()
+    )
+    return {media_type: count for media_type, count in counts}
+def get_user_media_identity(counts_dict):
+    if not counts_dict:
+        return "You are a Media Explorer"
+
+    top_type = max(counts_dict, key=counts_dict.get)
+    return {
+        'book': "You are a Reader",
+        'movie': "You are a Cinephile",
+        'music': "You are a Music Lover",
+        'tv_show': "You are a Binge-Watcher"
+    }.get(top_type, "You are a Media Explorer")
