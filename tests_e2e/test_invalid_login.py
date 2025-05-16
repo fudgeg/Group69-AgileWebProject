61a33d34806d97a21f56d393721fc2ef5be5c59d
Saayella <saayella@gmail.com>
2025-05-16 16:57:57 +0800

    2 selenium tests added

---
+++ tests_e2e/test_invalid_login.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+BASE = "http://127.0.0.1:5000"
+
+def test_invalid_login(driver):
+    driver.get(f"{BASE}/login")
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.NAME, "email")))
+
+    driver.find_element(By.NAME, "email").send_keys("nonexistent@example.com")
+    driver.find_element(By.NAME, "password").send_keys("wrongpass")
+    driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+    WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, ".popup-message.error")))
+    assert "Invalid email or password" in driver.page_source
